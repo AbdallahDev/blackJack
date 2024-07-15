79bbd12f880653f1ad1a449240fe91d7e80120ed
AbdallahDev <abdallah.dev85@gmail.com>
2024-07-15 13:07:03 +0300

    comment

--- main.py
+++ main.py
@@ -3,21 +3,53 @@ import art
 from random import choice
 
 
-def draw_card(player_cards):
+def draw_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = choice(cards)
-    if card == 11 and calculate_score(player_cards) + card > 21:
-        card = 1
     return card
 
 
+def set_final_status(u_score, c_score):
+    if c_score == 0:
+        return "You lose, Computer got a blackjack."
+    elif u_score == 0:
+        return "You win, You got a blackjack."
+    elif u_score > 21:
+        return "You went over, You Lose."
+    elif c_score > 21:
+        return "Opponent went over, You win."
+    elif u_score == c_score:
+        return "Draw"
+    elif u_score > c_score:
+        return "Win"
+    else:
+        return "Lose"
+
+
+def print_hands(cards1, cards2):
+    """print the players hands"""
+    print(f"    User cards: {cards1}, current score: {calculate_score(cards1)}")
+    print(f"    Computer first card: {cards2[0]}")
+
+
+def draw_computer_cards(cards):
+    """deal all the cards for the computer until it exceeds the score 16"""
+
+    return cards
+
+
 def calculate_score(cards):
-    score = 0
-    for card in cards:
-        score += card
+    """returns cards score"""
+    score = sum(cards)
+    if len(cards) == 2 and score == 21:
+        """this means the user go a blackjack"""
+        return 0
     if 11 in cards and score > 21:
-        cards[cards.index(11)] = 1
-        score = calculate_score(cards)
+        for card in cards:
+            if card == 11:
+                cards.remove(card)
+                cards.append(1)
+        calculate_score(cards)
     return score
 
 
@@ -28,49 +60,30 @@ def game():
     computer_cards = []
 
     for _ in range(2):
-        user_cards.append(draw_card(user_cards))
-        computer_cards.append(draw_card(computer_cards))
+        user_cards.append(draw_card())
+        computer_cards.append(draw_card())
 
-    # user_cards = [11, 9]
-    # computer_cards = [11, 9]
+    # user_cards = [11, 10]
+    computer_cards = [11, 10]
     user_score = calculate_score(user_cards)
     computer_score = calculate_score(computer_cards)
 
-    print(f"    User cards: {user_cards}, current score: {user_score}")
-    print(f"    Computer first card: {computer_cards[0]}")
-    if user_score == 21 or computer_score == 21:
-        while computer_score < 17:
-            computer_cards.append(draw_card(computer_cards))
-            computer_score = calculate_score(computer_cards)
-
-        if computer_score == 21:
-            win_lose_status = "You lose, Computer got a blackjack."
-        else:
-            win_lose_status = "You win, You got a blackjack."
+    print_hands(user_cards, computer_cards)
+    if user_score == 0 or computer_score == 0:
+        pass
     else:
         while input("Do you want another card?: ") == 'y':
-            user_cards.append(draw_card(user_cards))
+            user_cards.append(draw_card())
             user_score = calculate_score(user_cards)
-            print(f"    User cards: {user_cards}, current score: {user_score}")
-            print(f"    Computer first card: {computer_cards[0]}")
+            print_hands(user_cards, computer_cards)
             if user_score > 21:
                 break
 
-        computer_score = calculate_score(computer_cards)
         while computer_score < 17:
-            computer_cards.append(draw_card(computer_cards))
+            computer_cards.append(draw_card())
             computer_score = calculate_score(computer_cards)
 
-        if user_score > 21:
-            win_lose_status = "You went over, You Lose."
-        elif computer_score > 21:
-            win_lose_status = "Opponent went over, You win."
-        elif user_score == computer_score:
-            win_lose_status = "Draw"
-        elif user_score > computer_score:
-            win_lose_status = "Win"
-        else:
-            win_lose_status = "Lose"
+    win_lose_status = set_final_status(user_score, computer_score)
     print(f"    Your final hand: {user_cards}, final score: {user_score}")
     print(f"    Computer's final hand: {computer_cards}, final score: {computer_score}")
     print(win_lose_status)
